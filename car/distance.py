b36979d3ffa60526397558983ed9fb8b6f93d4e5
gyuu <gyuu.fuu@gmail.com>
2016-06-19 23:54:25 +0800

    distance

--- car/distance.py
+++ car/distance.py
@@ -20,8 +20,8 @@ def checkdist():
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
-        GPIO.setup(2, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(3, GPIO.IN)
+    GPIO.setup(2, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(3, GPIO.IN)
 
     time.sleep(2)
     try:
