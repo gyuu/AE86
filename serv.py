735eeb38cf656f02497ca009f525a04652c7aa4b
gyuu <gyuu.fuu@gmail.com>
2016-06-19 23:19:30 +0800

    servo control

--- serv.py
+++ serv.py
@@ -3,11 +3,13 @@ from flask import (
     render_template,
     request,
     jsonify,
-    )
+)
 
-from car import move
+from move import move
+from car.rotate import rotate
 
 app = Flask(__name__, template_folder=".")
+
 
 @app.route('/')
 def index():
@@ -16,12 +18,26 @@ def index():
 
 @app.route('/car', methods=['POST'])
 def car():
-	op = request.form['op']
-	if not op:
-		return jsonify(status='400', mesg='no op.')
-	else:
-		move(op)
-		return jsonify(status='200', mesg=op)
+    op = request.form['op']
+    if not op:
+        return jsonify(status='400', mesg='no op.')
+    else:
+        move(op)
+        return jsonify(status='200', mesg=op)
+
+
+@app.route('/servo', methods=['POST'])
+def servo():
+    op = request.form['op']
+    val = {
+        'up': -30,
+        'down': 150,
+    }
+    if not op:
+        return jsonify(status='400', mesg='no op.')
+    else:
+        rotate(val[op])
+        return jsonify(status='200', mesg=op)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
